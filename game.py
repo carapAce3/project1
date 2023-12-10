0f8b83610d462402fa7909606c8f528a1f96fc3e
carapAce3 <143933759+carapAce3@users.noreply.github.com>
2023-12-10 13:15:44 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -16,21 +16,33 @@ class Object(sprite.Sprite):
         keys = key.get_pressed()
         if keys[K_a] and self.rect.x > 0:
             self.rect.x -= self.speed
+            for w in walls:
+                if sprite.collide_rect(anti_mage,w):
+                    self.rect.x += self.speed
         if keys[K_d] and self.rect.x < 1000:
             self.rect.x += self.speed
+            for w in walls:
+                if sprite.collide_rect(anti_mage,w):
+                    self.rect.x -= self.speed
         if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 700:
+            for w in walls:
+                if sprite.collide_rect(anti_mage,w):
+                    self.rect.y += self.speed
+        if keys[K_s] and self.rect.y < 620:
             self.rect.y += self.speed
+            for w in walls:
+                if sprite.collide_rect(anti_mage,w):
+                    self.rect.y -= self.speed
 
     direction = 'right'
     def move2(self):
         if self.rect.x > 1000:
-            self.direction = 'left'
-        if self.rect.x < 20:
             self.direction = 'right'
+        if self.rect.x < -150:
+            self.direction = 'left'
         
-        if self.direction == 'right':
+        if self.direction == 'left':
             self.rect.x += self.speed
         else:
             self.rect.x -= self.speed
@@ -39,7 +51,7 @@ class Object(sprite.Sprite):
     def move3(self):
         if self.rect.y > 600:
             self.direction = 'down'
-        if self.rect.y < 100:
+        if self.rect.y < 220:
             self.direction = 'up'
         
         if self.direction == 'up':
@@ -71,14 +83,27 @@ class Wall(sprite.Sprite):
 
 
 window = display.set_mode((1100,700), RESIZABLE)
-picture = transform.scale(image.load("bg1.png"),(1100,700))
+picture = transform.scale(image.load("bg2.webp"),(1100,700))
 
-anti_mage = Object("anti-mage.png",80,300,100,100,10)
+anti_mage = Object("anti-mage.png",80,300,100,100,5)
 pudge = Object("pudge.png",800,50,100,100,4)
-Krip = Object("Krip1.webp",800,150,100,100,4)
+Krip = Object("Krip1.webp",500,150,100,100,4)
+Rune1 = Object("rune.png",1030,20,70,70,4)
+Rune2 = Object("rune.png",300,450,70,70,4)
 
-wall = Wall(0, 0, 0, 200, 200, 1000, 10)
-wall2 = Wall(0, 0, 0, 200, 200, 10, 1000)
+wall = Wall(0, 0, 0, 200, 200, 750, 40)
+wall2 = Wall(0, 0, 0, 200, 200, 40, 1000)
+wall3 = Wall(0, 0, 0, 200, 350, 250, 40)
+wall4 = Wall(0, 0, 0, 450, 350, 40, 250)
+wall5 = Wall(0, 0, 0, 230,560, 250, 40)
+wall6 = Wall(0, 0, 0, 450,550, 40, 250)
+walls = []
+walls.append(wall)
+walls.append(wall2)
+walls.append(wall3)
+walls.append(wall4)
+walls.append(wall5)
+walls.append(wall6)
 
 clock = time.Clock()
 #створення головного циклу
@@ -94,15 +119,26 @@ while game:
     anti_mage.reset()
     pudge.reset()
     Krip.reset()
+    Rune1.reset()
+    Rune2.reset()
     anti_mage.move()
     pudge.move2()
     Krip.move3()
     wall.draw_wall()
     wall2.draw_wall()
+    wall3.draw_wall()
+    wall4.draw_wall()
+    wall5.draw_wall()
+    wall6.draw_wall()
 
     if sprite.collide_rect(anti_mage, pudge) or sprite.collide_rect(anti_mage, Krip):
         game = False
-
+    if sprite.collide_rect(anti_mage, Rune1):
+        Rune1.rect.x = -500
+        wall3.rect.x = -1243
+    if sprite.collide_rect(anti_mage, Rune2):
+        Rune2.rect.x = -500
+        wall6.rect.x = -2347
 
  
     
