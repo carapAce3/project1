c6ad6e64779bf9142bac4b6118769c3579d9cc54
carapAce3 <143933759+carapAce3@users.noreply.github.com>
2024-02-04 13:06:49 +0200

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -1,5 +1,19 @@
 from pygame import *
+from random import randint
 '''Необхідні класи'''
+
+
+#музика
+mixer.init()
+#фонова музика
+mixer.music.load('odetari.mp3')
+mixer.music.play()
+
+#звук
+sound1 = mixer.Sound('sound.mp3')
+sound1.play()
+
+
  
 # клас-батько для спрайтів
 class GameSprite(sprite.Sprite):
@@ -40,13 +54,35 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
+picture = transform.scale(image.load("p1.png"),(600,500))
+picture2 = transform.scale(image.load("p2.png"),(600,500))
+
+
+#текст
+font.init()
+font1 = font.Font(None, 30) #шрифт і розмір тексту
+
+score1 = 0
+score2 = 0
+t = "Рахунок: " + str(score1) + ' - ' + str(score2)
+text = font1.render(t, True, (0,0,0))
+
+
+
+
 
 player1 = Player('racket.png', 0, 180, 10, 50, 100)
 player2 = Player('racket.png', 545, 180, 10, 50, 100)
 ball = GameSprite("ball1.png",300,200,10,60,60)
 
-x = 10
-y = 10
+x = 3
+y = 3
+
+
+window.fill(back)
+
+
+
 
 #прапорці, що відповідають за стан гри
 game = True
@@ -66,19 +102,30 @@ while game:
         player1.update_l()
         player2.update_r()
 
+        t = "Рахунок: " + str(score1) + ' - ' + str(score2)
+        text = font1.render(t, True, (0,0,0))
+        window.blit(text,(100,100))
+
         ball.rect.x -= x
         ball.rect.y += y
 
         if ball.rect.y > 450 or ball.rect.y < 0:
             y *= -1
         
-        if ball.rect.x < -40 or ball.rect.x > 600:
-            finish = False
-            game = False
-        
+        if ball.rect.x < -40:
+            ball.rect.x = 200
+            ball.rect.y = 200
+            score2 += 1
+        if ball.rect.x > 600:
+            ball.rect.x = 200
+            ball.rect.y = 200
+            score1 += 1
         if sprite.collide_rect(ball,player1) or sprite.collide_rect(ball,player2):
             x *= -1
-        
+        if score1 >= 3:
+            window.blit(picture,(0,0))
+        if score2 >= 3:
+            window.blit(picture2,(0,0))
 
 
         
