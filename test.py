83f4545ca2977b44cf67d8889d2329c9baf7fc11
carapAce3 <143933759+carapAce3@users.noreply.github.com>
2024-02-18 13:31:52 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,37 @@
-import random
-import string
-random.seed(10)
-letters = string.ascii_lowercase
-rand_letters = random.choices(letters,k=5) # where k is the number of required rand_letters
+from pygame import *
+import pygame_menu
 
-print(rand_letters)
+init()
+font = pygame_menu.font.FONT_MUNRO
+window = display.set_mode((600,400))
 
-['o', 'l', 'p', 'f', 'v']
+def start():
+    print('starting...')
+def end():
+    exit()
+def help():
+    mytheme = pygame_menu.Theme(widget_font=font,background_color=(0,0,0,0),
+                                title_background_color = (0,0,0))
+    
+    help_menu = pygame_menu.Menu('Головне меню', 600, 400, theme=mytheme)
+    help_menu.add.label('Information')
+    help_menu.add.label('Press [Start] to start')
+    help_menu.add.label('Press key [Stop] to stop')
+    
+    def back():
+        menu.enable()
+        help_menu.disable()
+        menu.mainloop(window)
+    help_menu.add.button('Назад', back)
+    menu.disable()
+    help_menu.mainloop(window)
+        
+
+
+menu = pygame_menu.Menu('Головне меню', 600, 400, theme=pygame_menu.themes.THEME_SOLARIZED.copy())
+menu.add.button('Запустити гру',start)
+menu.add.button('Завершити гру',end)
+menu.add.button('Допомога', help)
+
+
+menu.mainloop(window)
